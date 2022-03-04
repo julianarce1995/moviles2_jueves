8613d9dfa1cefe6602455a7f7fa1b0e409103ff2
Juan Zabala <juanzabaladev@gmail.com>
2022-03-03 20:46:40 -0500

    db config working

--- ApiRest/db/db_config.php
+++ ApiRest/db/db_config.php
@@ -1,2 +1,19 @@
 <?php
-echo "inicio proyecto";
+class DBConfig{
+
+    private $user = "root";
+    private $password = "";
+    private $dbName = "delivery_app";
+    private $host = "localhost";
+
+    public function connect(){
+        try{
+            $dsn = "mysql:host=$this->host;dbname=$this->dbName";
+            $connection = new PDO($dsn,$this->user, $this->password);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            echo "Conexión exitosa";
+        }catch(PDOException $exception){
+            echo "Error en la db". $exception->getMessage();
+        }
+    }
+}
