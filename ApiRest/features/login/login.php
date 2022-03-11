ad96147544a70fbe8d74b4b49684eea7517647ae
Juan Zabala <juanzabaladev@gmail.com>
2022-03-10 21:04:34 -0500

    get request complete

--- ApiRest/features/login/login.php
+++ ApiRest/features/login/login.php
@@ -1,6 +1,13 @@
 <?php
-if( $_SERVER['REQUEST_METHOD'] == "POST"){
-    echo "todo bien";
+require_once(dirname(__DIR__)."../../db/db_config.php");
+if( $_SERVER['REQUEST_METHOD'] == "GET"){
+    $identification = $_GET['identification'];
+    $db = new DBConfig();
+    $dbConnection = $db->connect();
+    $query = "SELECT * FROM users ";
+    $users = $dbConnection->query($query)->fetchAll(PDO::FETCH_ASSOC);
+    echo(json_encode($users));
+
 }
 else{
     echo "parce no se puede";
